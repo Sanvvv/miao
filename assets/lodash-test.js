ae7a55e29b82ccec732e57fe205c736c83926aef
xy <1050207224@qq.com>
2018-07-12 16:04:52 +0800

    fix

--- assets/lodash-test.js
+++ assets/lodash-test.js
@@ -1,31 +1,125 @@
 var test = {
-  chunk: {
-    i:  [['a', 'b', 'c', 'd'], 3],
-    e:  [['a', 'b', 'c'], ['d']]
-  },
+  chunk: [
+    {
+      i:  [['a', 'b', 'c', 'd'], 3],
+      e:  [['a', 'b', 'c'], ['d']]
+    }
+  ],
 
-  difference: {
-    i: [[1,2,3,4], [1,2], [3]],
-    e: [4]
-  },
+  difference: [
+    {
+      i: [[1,2,3,4], [1,2], [3]],
+      e: [4]
+    }
+  ],
 
-  differenceBy: {
-    i: [[{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x'],
-    e: [{ 'x': 2 }]
-  }
+  differenceBy: [
+    {
+      i: [[{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x'],
+      e: [{ 'x': 2 }]
+    }
+  ],
+
+  drop: [
+    {
+      i: [[1,2,3]],
+      e: [2,3]
+    },
+    {
+      i: [[1,2,3], 5],
+      e: []
+    },
+    {
+      i: [[1,2,3], 0],
+      e: [1,2,3]
+    }
+  ],
+
+  dropRight: [
+    {
+      i: [[1,2,3]],
+      e: [1,2]
+    },
+    {
+      i: [[1,2,3], 2],
+      e: [1]
+    },
+    {
+      i: [[1,2,3], 5],
+      e: []
+    }
+  ],
+
+  fill: [
+    {
+      i: [[4, 6, 8, 10], '*', 1, 3],
+      e: [4, '*', '*', 10]
+    },
+    {
+      i: [Array(3), 2],
+      e: [2,2,2]
+    }
+  ],
+
+  head: [
+    {
+      i: [[1,2,3]],
+      e: 1
+    }
+  ],
+
+  flatten: [
+    {
+      i: [[1, [2, [3, [4]], 5]]],
+      e: [1, 2, [3, [4]], 5]
+    }
+  ],
+
+  flattenDeep: [
+    {
+      i: [[1, [2, [3, [4]], 5]]],
+      e: [1,2,3,4,5]
+    }
+  ],
+
+  flattenDepth: [
+    {
+      i: [[1, [2, [3, [4]], 5]], 1],
+      e: [1, 2, [3, [4]], 5]
+    },
+    {
+      i: [[1, [2, [3, [4]], 5]], 2],
+      e: [1, 2, 3, [4], 5]
+    }
+  ],
+
+  fromPairs: [
+    {
+      i: [[['a', 1], ['b', 2]]],
+      e: { 'a': 1, 'b': 2 }
+    }
+  ]
 }
 
-for (let f in test) {
-  let expect = JSON.stringify(test[f].e)
-  let result = JSON.stringify(sanvvv[f](...test[f].i))
-  
-  if (expect !== result) {
-    console.log('-------------------------------')
-    console.log(f)
-    console.log('expect: ' + expect)
-    console.log('result: ' + result )
+for (let func in test) {
+  for (let f of test[func]) {
+    let expect = JSON.stringify(f.e)
+    let result = JSON.stringify(sanvvv[func](...f.i))
+
+    if (expect !== result) {
+      console.log('-------------------------------')
+      console.log(func)
+      console.log('input : ' + JSON.stringify(f.i))
+      console.log('expect: ' + expect)
+      console.log('result: ' + result )
+    }
   }
 }
 
 console.log('-------------------------------')
 console.log('complete')
+console.log('-------------------------------')
+
+// identity
+// var object = { 'a': 1 };
+// console.log(sanvvv.identity(object) === object);
--- lodash/sanvvv-lodash.js
+++ lodash/sanvvv-lodash.js
@@ -30,7 +30,7 @@ var sanvvv = {
 
     if (values) {
       for (var item of values) {
-        res = res.filter((element) => item.indexOf(element) === -1)
+        res = res.filter(element => item.indexOf(element) === -1)
       }
     }
     return res
@@ -59,4 +59,77 @@ var sanvvv = {
       return res
     } else return array.slice(0, array.length)
   },
+
+  drop: function (array, n = 1) {
+    if (n < 0) return array.slice(0, array.length)
+    else return array.slice(n, array.length)
+  },
+
+  dropRight: function (array, n = 1) {
+    if (n >= array.length) return []
+    else return array.slice(0, array.length - n)
+  },
+
+  fill: function (array, value, start = 0, end = array.length) {
+    for (var i = start; i < end; i++) {
+      array[i] = value
+    }
+    return array
+  },
+
+  flatten: function (array) {
+    var res = []
+
+    array.forEach(item => {
+      if (Array.isArray(item)) {
+        item.forEach(element => res.push(element))
+      } else res.push(item)
+    })
+
+    return res
+  },
+
+  flattenDeep: function (array) {
+    var res = []
+
+    iterateAndPush(array)
+    return res
+
+    function iterateAndPush (arr) {
+      arr.forEach(item => {
+        if (Array.isArray(item)) iterateAndPush(item)
+        else res.push(item)
+      })
+    }
+  },
+
+  flattenDepth: function (array, depth = 1) {
+    var res = []
+
+    iterateAndPush(array, 0)
+    return res
+
+    function iterateAndPush (arr, deep) {
+      arr.forEach(item => {
+        if (Array.isArray(item) && deep < depth) iterateAndPush(item, deep + 1)
+        else res.push(item)
+      })
+    }
+  },
+
+  fromPairs: function (pairs) {
+    var res = {}
+
+    pairs.forEach(item => res[item[0]] = item[1])
+    return res
+  },
+
+  head: function (array) {
+    return array[0]
+  },
+  
+  // util
+  identity: function (...value) {
+    return value[0]
+  }
 }
